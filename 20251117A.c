a9c1b8772858c6dc1f1976b38c399ff624020d23
Mikhail Wang <sillyn95@gmail.com>
2025-11-17 23:57:17 +0800

    Add files via upload

---
+++ 20251117A.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+    int main(){
+    int num,tem,i=0;
+    char hex[101];
+    scanf("%d",&num);
+    while(num > 0){
+        int reminder = num % 6;
+        hex[i++] = reminder+'0';
+        num = num /6;
+    }
+    for(int j = i-1;j>=0;j--){
+        printf("%c",hex[j]);
+    }
+
+    system("pause");
+    return 0;
+}
