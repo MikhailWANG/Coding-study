b6ef799a9f8466355923969f7f6600f0604d17d7
Mikhail Wang <sillyn95@gmail.com>
2025-11-17 23:59:13 +0800

    Update 20251117A.c

--- 20251117A.c
+++ 20251117A.c
@@ -1,3 +1,4 @@
+//Decimal to Hexadecimal Converter//
 #include<stdio.h>
 #include<stdlib.h>
     int main(){
@@ -11,8 +12,9 @@
     }
     for(int j = i-1;j>=0;j--){
         printf("%c",hex[j]);
-    }
+    }// We can use the loop to traverse the elements in array//
 
     system("pause");
     return 0;
+
 }
